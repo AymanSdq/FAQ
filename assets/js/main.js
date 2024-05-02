cbda10846e737389ba627ecb4ca8c74def70a24f
Ayman Sedqi <sedqiayman02@gmail.com>
2024-05-02 12:17:03 +0100

    Addid some Javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 
-    var openFAQ = document.querySelector('.ques-click');
-    
+    var openFAQ = document.querySelectorAll('.ques-click');
+    var allCallIcons = document.querySelectorAll(".icon_showhide")
+
 
     // Add a click event listener to it
-    openFAQ.addEventListener('click', function() {
-        var plus_open = '';
-        var min_close = ''
+    openFAQ.forEach(function(faq_quest) {
+        
+        faq_quest.addEventListener('click', () => {
+
+            var call_icons = faq_quest.querySelector(".icon_showhide");
+
+            var asnwerfaq = faq_quest.querySelector(".asnwerfaq");
+
+            if (call_icons.src.endsWith('icon-plus.svg')) {
+                allCallIcons.forEach(icon => {
+                    icon.src = '/assets/images/icon-plus.svg';
+                });
+                call_icons.src = '/assets/images/icon-minus.svg';
+            } else if (call_icons.src.endsWith('icon-minus.svg')) {
+                allCallIcons.forEach(icon => {
+                    icon.src = '/assets/images/icon-plus.svg';
+                });
+                call_icons.src = '/assets/images/icon-plus.svg';
+            }
+            
+
+
+
+        })
+
     });
 
 
